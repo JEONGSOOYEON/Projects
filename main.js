76a5a4497c5bd9bd6514c232fac9caa9fc872e3a
Chloe <78246605+JEONGSOOYEON@users.noreply.github.com>
2021-10-03 16:58:41 +0900

    match current date and time to input one
    localStorage에 저장된 object 중에 current time과 일치하는 object를 새로운 array로 생성

--- main.js
+++ main.js
@@ -3,15 +3,19 @@ const inputTime = document.querySelector("#time");
 const inputText = document.querySelector("#text");
 const submit = document.querySelector("#submit");
 
-inputDate.value=new Date().toISOString().slice(0, 10);
-inputTime.value=new Date().toTimeString().slice(0, 5);
-
 let toDos = [];
 
-function saveTodos(todoObj) {
-    localStorage.setItem("todos", JSON.stringify(todoObj));
-    toDos.push(todoObj)
-    console.log(toDos)
+const year = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const date = new Date().getDate();
+const hrs = new Date().getHours();
+const min = new Date().getMinutes();
+
+const YMD = `${year}-${month < 10 ? `0${month}` : month}-${date < 10 ? `0${date}` : date}`;
+const HM = `${hrs < 10 ? `0${hrs}` : hrs}:${min < 10 ? `0${min}` : min}`;
+
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
 }
 
 function handleTodoSubmit(event) {
@@ -21,8 +25,27 @@ function handleTodoSubmit(event) {
         'time': inputTime.value,
         'text': inputText.value
     }
+    toDos.push(todoObj);
     inputText.value = "";
-    saveTodos(todoObj); 
+    saveTodos(); 
 }
 
 submit.addEventListener("click", handleTodoSubmit);
+
+const savedToDos = localStorage.getItem("todos");
+const parsedToDos = JSON.parse(savedToDos)
+let toDoDate = [];
+    for (let i = 0; i < parsedToDos.length; i++) {
+        if (parsedToDos[i].date === YMD &&
+            parsedToDos[i].time === HM) {
+            toDoDate.push(parsedToDos[i]);
+        }
+    }
+
+console.log(toDoDate)
+
+
+
+
+
+
