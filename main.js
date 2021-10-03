995ff9393a32e877a253c218fe60ca8b0a32ec66
Chloe <78246605+JEONGSOOYEON@users.noreply.github.com>
2021-10-03 19:50:47 +0900

    alert todo
    조건에 맞는 todo를 alert

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const inputTime = document.querySelector("#time");
 const inputText = document.querySelector("#text");
 const submit = document.querySelector("#submit");
 
-let toDos = [];
-
 const year = new Date().getFullYear();
 const month = new Date().getMonth() + 1;
 const date = new Date().getDate();
@@ -13,6 +11,11 @@ const min = new Date().getMinutes();
 
 const YMD = `${year}-${month < 10 ? `0${month}` : month}-${date < 10 ? `0${date}` : date}`;
 const HM = `${hrs < 10 ? `0${hrs}` : hrs}:${min < 10 ? `0${min}` : min}`;
+
+inputDate.value = new Date().toISOString().slice(0, 10);
+inputTime.value = new Date().toTimeString().slice(0, 5);
+
+let toDos = [];
 
 function saveTodos() {
     localStorage.setItem("todos", JSON.stringify(toDos));
@@ -33,16 +36,19 @@ function handleTodoSubmit(event) {
 submit.addEventListener("click", handleTodoSubmit);
 
 const savedToDos = localStorage.getItem("todos");
-const parsedToDos = JSON.parse(savedToDos)
-let toDoDate = [];
+const parsedToDos = JSON.parse(savedToDos);
+let dateToDos = [];
     for (let i = 0; i < parsedToDos.length; i++) {
         if (parsedToDos[i].date === YMD &&
             parsedToDos[i].time === HM) {
-            toDoDate.push(parsedToDos[i]);
+            dateToDos.push(parsedToDos[i]);
+            let result = dateToDos.map((dateToDo) => dateToDo.text);
+            alert(result);
         }
     }
 
-console.log(toDoDate)
+
+
 
 
 
